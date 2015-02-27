c04eff8135bc6f2d2a877eca303c3dfc24b52d82
Larissa Laich <la8895@web.de>
2015-02-27 18:44:40 +0100

    location request working

--- pythonServer/location/views.py
+++ pythonServer/location/views.py
@@ -5,7 +5,11 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from artist.models import Artist
 from artist.serializers import ArtistSerializer
+#for python3
+import urllib.request
 import logging
+import json
+
 
 logger = logging.getLogger(__name__)
 
@@ -16,16 +20,22 @@ logger = logging.getLogger(__name__)
 def getArtistList(request,latitude=1,longitude=1, format=None):
      artists = Artist.objects.all()
      serializer = ArtistSerializer(artists, many = True)
+     getNearbyPlaces(latitude,longitude)
      return Response(serializer.data)
 
 #get nearbyPlaces using the geonames api
 def getNearbyPlaces(latitude,longitude):
-    # http://api.geonames.org/findNearbyPlaceName?lat=48.639291&lng=9.135565&radius=300&maxRow=50&cities=cities15000&username=loadFromGitIgnore
-     loadGeoUsername()
+     request = 'http://api.geonames.org/findNearbyPlaceNameJSON?lat='+latitude +'&lng='+longitude+'&radius=300&maxRow=15&cities=cities15000&username=' + loadGeoUsername()
+     response = urllib.request.urlopen(request)
+     data = json.loads(response.read().decode("utf-8"))
      logger.error('test')
+     logger.error(data["geonames"][0]["toponymName"])
+
+
+
 
 def loadGeoUsername():
      with open ("username.txt", "r") as myfile:
           username=myfile.read()
-     logger.error(username)
+     #logger.error(username)
      return username
