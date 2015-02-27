999019d08a1f0628292e00b8d41170fdbc5a4ce0
Larissa Laich <la8895@web.de>
2015-02-27 17:20:52 +0100

    test

--- pythonServer/location/views.py
+++ pythonServer/location/views.py
@@ -5,9 +5,9 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from artist.models import Artist
 from artist.serializers import ArtistSerializer
+import logging
 
-
-
+logger = logging.getLogger(__name__)
 
 # Create your views here.
 
@@ -21,4 +21,11 @@ def getArtistList(request,latitude=1,longitude=1, format=None):
 #get nearbyPlaces using the geonames api
 def getNearbyPlaces(latitude,longitude):
     # http://api.geonames.org/findNearbyPlaceName?lat=48.639291&lng=9.135565&radius=300&maxRow=50&cities=cities15000&username=loadFromGitIgnore
+     loadGeoUsername()
+     logger.error('test')
 
+def loadGeoUsername():
+     with open ("username.txt", "r") as myfile:
+          username=myfile.read()
+     logger.error(username)
+     return username
